15c3c3b75b887d3d8489643269e9b1d13bf53928
Nick <nicholassimpson232@gmail.com>
2020-01-17 22:37:21 -0600

    added index

---
+++ index.js
@@ -0,0 +1,49 @@
+const octokit = require('@octokit/rest');
+const fs = require('fs');
+
+const username = "name of repo. owner";
+const user = octokit({
+	auth: "personal access token"
+});
+
+// writes file
+function write(path, content) {
+	fs.writeFile(path, content, (err) => {
+		if (err) throw Error(err);
+	});
+};
+
+// recursively builds file system
+async function build(src, path="") {
+
+	let repo = await user.repos.getContents({
+		owner: username,
+		repo: src,
+		path: path
+	});
+	let data = repo.data;
+
+	for (let item of data) {
+		if (item.type == "file") {
+			let file = await user.repos.getContents({
+				owner: username,
+				repo: src,
+				path: item.path
+			});
+			let content = Buffer.from(file.data.content, 'base64').toString();
+			write(item.path, content);
+		} else if (item.type == "dir") {
+			build(src, item.path);
+		};
+	};
+};
+
+async function init(src, interval=30000) {
+	if (!src) throw Error("provide src");
+	build(src);
+	setInterval(() => {
+		build(src);
+	}, interval);
+};
+
+module.exports = init;
